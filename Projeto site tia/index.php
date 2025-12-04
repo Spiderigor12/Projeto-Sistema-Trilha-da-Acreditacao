e5687a6212f88b048be03b4cf158ff42a33358c7
Igor Silva <igormspsbr@gmail.com>
2025-12-04 20:22:16 -0300

    Add files via upload

---
+++ Projeto site tia/index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Sistema</title>
+<link rel="stylesheet" href="style.css">
+
+<!--bot css-->
+<div class="carousel">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="icon" href="img/logo.ico">
+
+    <!--bot css-->
+    <!--bot js-->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+  </div>
+    <!--bot js-->
+    <!--Tag Responsiva-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Tag Responsiva-->
+    <meta charset="utf-8">
+    
+    <style>
+        iframe{max-width: 85%; max-height:auto; }
+    </style>
+    
+    </head>
+<body>
+    <header class="fundo">
+    <section div="col1"><img class="logo" src="img/logo.png" ></section></div>
+      <div class="menu-responsivo"> 
+        <div class="linha1"></div>
+        <div class="linha2"></div>
+        <div class="linha3"></div>
+    </div>
+
+<ul class="dp-menu">
+   <li><a href="#">HOME</a></li>
+    <li><a href="index.php#quemsomos">QUEM SOMOS</a></li>
+    <li><a href="index.php#oquefazemos">O QUE FAZEMOS</a></li>
+    <li><a href="index.php#nossamissao">NOSSOS PRINCÍPIOS</a></li>
+    <li><a href="index.php#porquecontratar">PORQUÊ CONTRATAR?</a></li>
+    <li><a href="index.php#contato">CONTATO</a></li>
+    <li><a href="#">CURSOS</a></li>
+</ul>
+
+</header>
+<!--Bot carrousel-->
+
+<br><br>
+<section class="galeria" id="#inicio">
+  <p>
+    <span>Contribuímos no desenvolvimento da Gestão da Qualidade da sua organização de saúde proporcionando soluções customizadas á sua realidade, mediante análise dos seus objetivos e recursos, 
+    visando à entrega de valor ao cliente.</span>
+  </p>
+<div class="imagens">
+      <img src="img/carrousel 1.jpg" alt="Imagem 1">
+      <img src="img/carrousel 2.jpg" alt="Imagem 1">
+      <img src="img/carrousel 3.jpeg" alt="Imagem 1">
+  </div>
+ 
+</section>
+<!--Fim do Carrossel-->
+
+<!--Quem Somos-->
+
+<section class="quemsomos" id="quemsomos">
+  <font color="#103c5c"><h1>QUEM SOMOS</h1></font>
+  </div>  
+  <div class="quemsomos1">
+
+<div class="anaimg">
+<center><img src="img/Ana.jpeg"></center>
+</div>
+<center><h2>Ana Lúcia Ponte</h2></center>
+
+Farmacêutica Bioquímica.
+<br><br>
+<p>MBA em Gestão da Qualidade e Segurança do Paciente
+Especialista 
+<br>
+em Acreditação de Qualidade nos serviços de saúde e em Segurança
+<br> 
+do Paciente
+Avaliadora da Organização Nacional de Acreditação (ONA)<br>
+Auditora Líder do Sistema Nacional de Acreditação - DICQ
+para 
+<br>Laboratórios Clínicos.
+<br><br>
+Consultora em implantação e desenvolvimento do Sistema de 
+<br>Gestão da Qualidade em Saúde.</p>
+<br>
+</div>
+ 
+  <div class="quemsomos2">
+  <div class="mackimg">
+<center><img src="img/Mack.jpeg"></center>
+  </div> 
+<h1>    </h1> 
+<center><h2>Macklaine Soares</h2></center>
+
+Farmacêutica Bioquímica
+<br><br>
+<p> Especialista em Acreditação de Qualidade nos serviços de saúde
+<br>e em Segurança do Paciente
+Avaliadora pela Acreditação Internacional
+<br>em saúde ACSA 
+Auditora Líder do Sistema Nacional de Acreditação
+<br> - DICQ para Laboratórios Clínicos.
+Analista de Perfil Comportamental e 
+<br>Produtividade Corporativa
+Coach Business/Executive Coach para lideranças
+<br> na área da Saúde.
+<br><br>
+Consultora em implantação e desenvolvimento do Sistema de<br>
+Gestão da Qualidade em Saúde.</p>
+</div>
+<br><br><br><br>
+ </section>
+ 
+<!--O que fazemos-->
+
+<section class="oquefazemos" id="oquefazemos">
+  <br><br>
+  <CENTER><font color="#103c5c"><h1>O QUE FAZEMOS?</h1></CENTER></font>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eligendi aliquid nisi sapiente quo ad minima accusantium sequi harum modi quae, dolor eos officia laboriosam, provident ducimus? Eos, delectus sapiente.
+</section>
+
+<!--Nossa Missao-->
+<section class="nossamissao" id="nossamissao">
+  <br><CENTER><font color="#103c5c"><h1>NOSSOS PRINCÍPIOS</h1></CENTER></font> <div class="missao"></div>
+ 
+    <div class="linha-mvv">
+      <div class="fundo2">
+      <div class="cou-missao">
+        
+        <span><p class="t-missao"><font color="#103c5c"></font>MISSÃO<br><img src="img/lacthosa-blue-icono-mision.png"><br>Proporcionar conhecimento e ferramentas para contribuir com a melhoria contínua dos Serviços de saúde em Acreditação.</p>
+          
+      </div><br>
+      <div class="cou-visao">
+          <p class="t-visao">VISÃO<br><img src="img/visao.png"><br>Ser excelência em desenvolvimento de organizações em Acreditação e Gestão em Saúde.</p>
+      </div><br>
+      <div class="cou-valores">
+          <p class="t-valores">VALORES<br><img src="img/pngwing.com.png"><br>Eficiência, confidencialidade, criatividade, parceria, qualidade, valorização do ser e compromisso Social
+            </p></span>
+      </div>
+</div>
+  </div>
+</div>
+
+</div>
+</section>
+
+<!--Porque contratar-->
+<section class="porquecontratar" id="porquecontratar"><br>
+  <CENTER><font color="#fffff"><h1>PORQUE NOS CONTRATARMOS?</h1></CENTER></font>
+  <center><p><span>Nós do Instituto estaremos junto com você e sua equipe nas trilhas da acreditação ou <br> simplesmente na adequação do serviço 
+    perante a legislação vigente, porém de forma leve e simplificada com segurança e eficiência.</span></p></center><br><br>
+
+    <div class="linha-mvv">
+      <div class="cou-motivo1">
+       
+        <p class="t-motivo1"><img src="img/confere.png" alt=""> COMPROMISSO COM RESULTADO</p> 
+          
+          <p class="c-motivo1">Proporcionamos melhorias e resultados mensuráveis para sua organização de saúde.</p>
+      
+      </div><br>
+      <div class="cou-motivo2">
+          <p class="t-motivo2"><img src="img/confere.png" alt=""> EQUIPE ESPECIALIZADA</p>
+          <p class="c-motivo2">Nossa equipe possui a expertise necessária para apoiar sua organização para conquista da  Acreditação.</p>
+      </div><br>
+      <div class="cou-motivo3">
+          <p class="t-motivo3"><img src="img/confere.png" alt=""> FORMATO ADAPTÁVEL</p>
+          <p class="c-motivo3">Consultoria personalizada, adaptada à sua rotina. Podendo ser Presencial, hibrida ou online, sempre ao seu lado durante toda jornada.</p>
+      </div>
+
+  </div>
+</section>
+
+<!--Contato-->
+<section class="contato" id="contato">
+  <h1>CONTATO</h1><br>
+  <form>
+    <fieldset>
+      <div class="formulario">
+        <div>
+      
+        <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo" required>
+      </div>
+      <div>
+       <br>
+      <input type="email" id="email" name="email" placeholder="Digite seu email" required>
+    </div><br>
+      <div class="formulario2">
+
+        <input class="metade" type="text" id="telefone" name="telefone" placeholder="Telefone: (00) 0000-0000" required>
+        
+        
+        <input class="metade" type="text" id="celular" name="celular" placeholder="Celular: (00) 00000-0000" required>
+      </div>
+    </div>
+    <br>
+      <textarea id="comentario" name="comentario" placeholder="Digite seu comentário" required></textarea>
+    </fieldset>
+    <div class="botao">
+      <input type="submit" value="Enviar">
+      <input type="reset" value="Limpar">
+    </div>
+ 
+  </form>
+  </center><br><br><br>
+
+</section>
+      <!-- Rodapé do site-->
+      <footer class="footer-distributed">
+
+        <div class="footer-left">
+  
+          <h3>Instituto<span> Trilhas da Acreditação</span></h3>
+  
+          <p class="footer-links">
+                  <a href="index.php">Home</a>
+                  <a href="index.php#quemsomos">Quem Somos</a>
+                  <a href="index.php#oquefazemos">O que Fazemos</a>
+                  <a href="index.php#nossamissao">Nossa Missão</a>
+                  <a href="index.php#porquecontratar">Porquê Contratar?</a>
+                  <a href="index.php#contato">Contato</a>
+          </p>
+  
+          <p class="footer-company-name">Instituto Trilhas da Acreditação © 2022</p>
+        </div>
+  
+        <div class="footer-center">
+  
+          <div>
+            <i class="fa fa-map-marker"></i>
+            <p> Santos - SP</p>
+          </div>
+  
+          <div>
+            <i class="fa fa-phone"></i>
+            <p>+55 13 98807-8730</p>
+          </div>
+  
+          <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:trilhasacreditacao@gmail.com">trilhasacreditacao@gmail.com</a></p>
+          </div>
+  
+        </div>
+  
+        <div class="footer-right">
+  
+          <p class="footer-company-about">
+            <span>Sobre nós</span>
+            Contribuímos no desenvolvimento da Gestão da Qualidade da sua organização de saúde proporcionando soluções customizadas á sua realidade, mediante análise dos seus objetivos e recursos, 
+            visando à entrega de valor ao cliente.</p>
+                  <br>
+  
+            <a href="https://api.whatsapp.com/send/?phone=551113996533712&text&type=phone_number&app_absent=0"  target="_blank">
+                      <img class="whatsapp" src="img/whatsapp.png"></a>
+             <a href="https://www.instagram.com/institutotrilhas.acreditacao/"  target="_blank">
+                       <img class="instagram" src="img/Instagram.png"></a>
+             <a href="https://www.linkedin.com/in/instituto-trilhas-da-acredita%C3%A7%C3%A3o-5bb71b255/"  target="_blank">
+                       <img class="whatsapp" src="img/linkedin.png"></a>
+        </div>
+  
+      </footer>
+  
+      
+    </body>
+        </html>
